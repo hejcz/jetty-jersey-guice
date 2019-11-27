9f5e6423e334a7cacd6c3de6d292768f73ed52fc
Julian Rubin <julian.rubin@rtbhouse.com>
2019-11-27 13:08:42 +0100

    Reformat App

--- src/main/java/com/github/hejcz/App.java
+++ src/main/java/com/github/hejcz/App.java
@@ -8,12 +8,14 @@ import org.glassfish.jersey.servlet.ServletProperties;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        context.setContextPath("/");
         ServletHolder servletHolder = new ServletHolder(ServletContainer.class);
         servletHolder.setInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, Config.class.getName());
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        context.setContextPath("/");
         context.addServlet(servletHolder, "/*");
+
+        Server server = new Server(8080);
         server.setHandler(context);
         server.start();
     }
